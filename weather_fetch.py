1cd8b8b7a1dbb77117b81363d10956de358f99fe
Nicholas Lombardo <lombrado.nicholas77@gmail.com>
2024-04-04 14:33:48 -0400

    first commit

---
+++ weather_fetch.py
@@ -0,0 +1,43 @@
+from urllib.request import urlopen
+from fastapi import FastAPI
+import pgeocode
+import json
+
+app = FastAPI()
+
+
+@app.get("/")
+def get_weather(zip: str):
+    return fetch_weather(zip)
+
+
+def fetch_weather(zip_code):
+    # TODO: make sure these are valid coordinates
+    nomi = pgeocode.Nominatim('us')
+    query = nomi.query_postal_code(zip_code)
+
+    data = {
+        "lat": query["latitude"],
+        "lon": query["longitude"]
+    }
+
+    lat = data["lat"]
+    long = data["lon"]
+
+    # these are the variables that are requested
+    # from the api
+    vars = [
+        "temperature_2m",
+        "relative_humidity_2m",
+        "apparent_temperature",
+        "precipitation_probability",
+        "precipitation",
+        "snow_depth",
+        "cloud_cover",
+        "wind_speed_10m"
+    ]
+
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={long}&current={','.join(vars)}&temperature_unit=fahrenheit"
+    response = urlopen(url)
+    data_json = json.loads(response.read())
+    return data_json["current"]
